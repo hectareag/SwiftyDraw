b027425ca6b645b6cc7554b9d528a008a5f23a01
fanquake <fanquake@gmail.com>
2018-08-25 23:47:12 +0800

    carthage: Add SwiftyDraw.h and share .xcsheme
    Test carthage builds using: carthage build --no-skip-current

---
+++ SwiftyDraw/SwiftyDraw.h
@@ -0,0 +1,17 @@
+//
+//  SwiftyDraw.h
+//  SwiftyDraw
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDraw.
+FOUNDATION_EXPORT double SwiftyDrawVersionNumber;
+
+//! Project version string for SwiftyDraw.
+FOUNDATION_EXPORT const unsigned char SwiftyDrawVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDraw/PublicHeader.h>
+
+
